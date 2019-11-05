c4f6587699d05659f9dcd2c6a94884902c934442
Matt Wamboldt <matt@heroless.com>
2019-11-04 22:27:28 -0400

    Couple style changes to have full layout

--- src/components/player.tsx
+++ src/components/player.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 const Player = () => {
     return (
         <div id="player">
-            <canvas id="visualizer" width="500px" height="500px"></canvas>
+            <canvas id="visualizer" width="300px" height="300px"></canvas>
             <audio id="audio" controls>
                 <source id="audioSource" src="audio/celebration.mp3" type="audio/mp3" />
                 Your browser does not support the audio tag.
